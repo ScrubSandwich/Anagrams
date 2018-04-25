6ffac0b0ef805fe47da48ab4889a700353f31be0
jmieczni <jmieczni@purdue.edu>
2018-04-25 16:25:03 -0400

    Get words from command line arguments

---
+++ Anagrams.java
@@ -0,0 +1,10 @@
+public class Anagrams {
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.out.println("Usage: Anagrams word1 word2");
+      System.exit(0);
+    }
+    String word1 = args[0];
+    String word2 = args[2];
+  }
+}
